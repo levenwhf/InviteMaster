955bbfc1db45cfde87aa3e174a94c59935031474
leven <weihongfang@weihongfangdeimac.local>
2018-09-07 15:44:34 +0800

    创建登录页面

---
+++ InviteMaster/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  InviteMaster
+//
+//  Created by 韦洪方 on 2018/9/7.
+//  Copyright © 2018年 Leven Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
